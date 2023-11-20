ed3dcfa0c3b51388f0e62f1e8ec350a2cbd3db3b
Ace <krapahbless@gmail.com>
2023-11-20 09:19:38 +0000

    Testing_with_pytest

---
+++ testing_with_pytest/testing_functions/name_function.py
@@ -0,0 +1,21 @@
+def get_formatted_name(first,last,middle=None):
+    """Generate a neatly formatted full name."""
+    if middle:
+        full_name = f"{first} {middle} {last}"
+    else:
+        full_name = f"{first} {last}"
+
+    return full_name.title()
+
+'''
+print("Enter 'q' at any time to quit.")
+while True:
+    first = input("\nPlease give me a first name: ")
+        if first == 'q':
+        break
+    last = input("Please give me a last name: ")
+        if last == 'q':
+            break
+formatted_name = get_formatted_name(first, last)
+print(f"\tNeatly formatted name: {formatted_name}.")
+'''
