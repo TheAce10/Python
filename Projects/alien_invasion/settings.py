8d0f024da7f522f110914df11eb941beb3778a08
Ace <krapahbless@gmail.com>
2023-11-20 16:25:29 +0000

    20.11.23

--- Projects/alien_invasion/settings.py
+++ Projects/alien_invasion/settings.py
@@ -9,10 +9,11 @@ class Settings:
         self.bg_color = (230, 230, 230)
 
         # Ship settings
-        self.ship_speed = 1.5
+        self.ship_speed = 5.5
 
         # Bullet settings
         self.bullet_speed = 4.0
-        self.bullet_width = 3
-        self.bullet_height = 15
+        self.bullet_width = 2
+        self.bullet_height = 10
         self.bullet_color = (60, 0, 0)
+        self.bullets_allowed = 10
