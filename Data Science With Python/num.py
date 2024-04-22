5a74dbce5e1a839f1b67472ff6bab463c7f40a99
Bless Elikem Krapah <krapahbless@gmail.com>
2024-04-22 23:11:06 +0000

    22.04.24

--- Data Science With Python/num.py
+++ Data Science With Python/num.py
@@ -1,9 +1,20 @@
-import numpy as nm
+import numpy as np
 
 height = [1,2,3,4,5]
-width = [2,4,6,8,10]
+weight = [2,4,6,8,10]
 np_height = np.array(height)
-np_width = np.array(width)
+np_weight = np.array(weight)
 
-bmi = np_width/np_height**2
-print(bmi)
+bmi = np_weight/np_height**2
+# print(bmi)
+
+np_2d = np.array([[1],[2]])
+print(np_2d[0,0])
+
+h = np.random.normal(1, 0.2, 20)
+cs = np.column_stack((np_height, np_weight))
+
+# np.column_stack((h,h))
+
+print(np.corrcoef(cs))
+
