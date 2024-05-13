6b4335099dc940c28605676cec2fb5d5141831c7
Bless Elikem Krapah <krapahbless@gmail.com>
2024-05-13 21:49:20 +0000

    2

--- leetcode/1.py
+++ leetcode/1.py
@@ -1,7 +1,13 @@
-def solution(num):
-    pdt, summ = (1, 0)
-    for i in str(num):
-        pdt *= int(i)
-        summ += int(i)
-    return pdt-summ
-print(solution(4421))
+# def solution(num):
+#     pdt, summ = (1, 0)
+#     for i in str(num):
+#         pdt *= int(i)
+#         summ += int(i)
+#     return pdt-summ
+# print(solution(4421))
+
+print(1%10)
+def test(n, *args):
+    print(n)
+
+test(1,2,3,4,5)
